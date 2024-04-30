d42b20dd943a514568789721f6615a20c8ec47be
Eui-kwon MOON <code1009@naver.com>
2024-04-30 20:54:33 +0900

    1

--- application/source/res/resource.h
+++ application/source/res/resource.h
@@ -4,131 +4,40 @@
 //
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-#ifndef IDC_STATIC
-#define IDC_STATIC                      -1
-#endif
-
-#ifndef RT_MANIFEST
-#define RT_MANIFEST                     24
-#endif
-
-#ifndef SC_SIZE
-#define SC_SIZE                         0xF000
-#define SC_MOVE                         0xF010
-#define SC_MINIMIZE                     0xF020
-#define SC_MAXIMIZE                     0xF030
-#define SC_NEXTWINDOW                   0xF040
-#define SC_PREVWINDOW                   0xF050
-#define SC_CLOSE                        0xF060
-#define SC_VSCROLL                      0xF070
-#define SC_HSCROLL                      0xF080
-#define SC_MOUSEMENU                    0xF090
-#define SC_KEYMENU                      0xF100
-#define SC_ARRANGE                      0xF110
-#define SC_RESTORE                      0xF120
-#define SC_TASKLIST                     0xF130
-#define SC_SCREENSAVE                   0xF140
-#define SC_HOTKEY                       0xF150
-#endif
+#include <res/wxx/resource_wxx.h>
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-#define IDW_MAIN                        41
-#define IDW_ABOUT                       42
-#define IDW_VIEW_TOOLBAR                43
-#define IDW_VIEW_STATUSBAR              44
-#define IDW_READY                       45
-#define IDW_INDICATOR_CAPS              46
-#define IDW_INDICATOR_NUM               47
-#define IDW_INDICATOR_SCRL              48
-#define IDW_MENUBAR                     49
-#define IDW_TOOLBAR                     50
-#define IDW_QUIT                        51
-#define IDW_MDI_CASCADE                 52
-#define IDW_MDI_TILE                    53
-#define IDW_MDI_ARRANGE                 54
-#define IDW_MDI_CLOSEALL                55
-#define IDW_FIRSTCHILD                  56
-#define IDW_CHILD2                      57
-#define IDW_CHILD3                      58
-#define IDW_CHILD4                      59
-#define IDW_CHILD5                      60
-#define IDW_CHILD6                      61
-#define IDW_CHILD7                      62
-#define IDW_CHILD8                      63
-#define IDW_CHILD9                      64
-#define IDW_CHILD10                     65
-#define IDW_FILE_MRU_FILE1              66
-#define IDW_FILE_MRU_FILE2              67
-#define IDW_FILE_MRU_FILE3              68
-#define IDW_FILE_MRU_FILE4              69
-#define IDW_FILE_MRU_FILE5              70
-#define IDW_FILE_MRU_FILE6              71
-#define IDW_FILE_MRU_FILE7              72
-#define IDW_FILE_MRU_FILE8              73
-#define IDW_FILE_MRU_FILE9              74
-#define IDW_FILE_MRU_FILE10             75
-#define IDW_FILE_MRU_FILE11             76
-#define IDW_FILE_MRU_FILE12             77
-#define IDW_FILE_MRU_FILE13             78
-#define IDW_FILE_MRU_FILE14             79
-#define IDW_FILE_MRU_FILE15             80
-#define IDW_FILE_MRU_FILE16             81
-#define IDW_SPLITH                      82
-#define IDW_SPLITV                      83
-#define IDW_TRACK4WAY                   84
-#define IDW_SDBOTTOM                    85
-#define IDW_SDCENTER                    86
-#define IDW_SDLEFT                      87
-#define IDW_SDMIDDLE                    88
-#define IDW_SDRIGHT                     89
-#define IDW_SDTOP                       90
-#define IDW_PRINTPREVIEW                91
-#define IDW_PREVIEWPRINT                92
-#define IDW_PREVIEWSETUP                93
-#define IDW_PREVIEWPREV                 94
-#define IDW_PREVIEWNEXT                 95
-#define IDW_PREVIEWCLOSE                96
-#define IDW_PREVIEWPANE                 97
-
-/////////////////////////////////////////////////////////////////////////////
-//===========================================================================
-#define IDM_FILE_NEW                    101
-#define IDM_FILE_OPEN                   102
-#define IDM_FILE_SAVE                   103
-#define IDM_FILE_SAVEAS                 104
-#define IDM_FILE_PRINT                  105
-#define IDM_FILE_CLOSE                  106
-#define IDM_FILE_EXIT                   107
-#define IDM_EDIT_UNDO                   110
-#define IDM_EDIT_REDO                   111
-#define IDM_EDIT_CUT                    112
-#define IDM_EDIT_COPY                   113
-#define IDM_EDIT_PASTE                  114
-#define IDM_EDIT_DELETE                 115
-#define IDM_HELP_ABOUT                  120
-#define IDM_CONTAINER_TOP               131
-#define IDM_TABBEDMDI_TOP               132
-#define IDM_HIDE_SINGLE_TAB             133
-#define IDM_DEFAULT_LAYOUT              134
-#define IDM_CLOSE_DOCKERS               135
-#define IDM_CLOSE_MDIS                  136
+#define IDM_FILE_NEW                    201
+#define IDM_FILE_OPEN                   202
+#define IDM_FILE_SAVE                   203
+#define IDM_FILE_SAVEAS                 204
+#define IDM_FILE_PRINT                  205
+#define IDM_FILE_CLOSE                  206
+#define IDM_FILE_EXIT                   207
+#define IDM_EDIT_UNDO                   210
+#define IDM_EDIT_REDO                   211
+#define IDM_EDIT_CUT                    212
+#define IDM_EDIT_COPY                   213
+#define IDM_EDIT_PASTE                  214
+#define IDM_EDIT_DELETE                 215
+#define IDM_HELP_ABOUT                  220
+#define IDM_CONTAINER_TOP               231
+#define IDM_TABBEDMDI_TOP               232
+#define IDM_HIDE_SINGLE_TAB             233
+#define IDM_DEFAULT_LAYOUT              234
+#define IDM_CLOSE_DOCKERS               235
+#define IDM_CLOSE_MDIS                  236
 #define IDM_NEWMENU                     200
-
-/////////////////////////////////////////////////////////////////////////////
-//===========================================================================
-#define IDB_TOOLBAR16                   1001
-#define IDI_DOCKER                      1002
-#define IDI_VIEW                        1003
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        200
+#define _APS_NEXT_RESOURCE_VALUE        300
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         2000
-#define _APS_NEXT_SYMED_VALUE           201
+#define _APS_NEXT_SYMED_VALUE           301
 #endif
 #endif
---
+++ application/source/res/wxx/resource_wxx.h
@@ -0,0 +1,94 @@
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#ifndef IDC_STATIC
+#define IDC_STATIC                      -1
+#endif
+
+#ifndef RT_MANIFEST
+#define RT_MANIFEST                     24
+#endif
+
+#ifndef SC_SIZE
+#define SC_SIZE                         0xF000
+#define SC_MOVE                         0xF010
+#define SC_MINIMIZE                     0xF020
+#define SC_MAXIMIZE                     0xF030
+#define SC_NEXTWINDOW                   0xF040
+#define SC_PREVWINDOW                   0xF050
+#define SC_CLOSE                        0xF060
+#define SC_VSCROLL                      0xF070
+#define SC_HSCROLL                      0xF080
+#define SC_MOUSEMENU                    0xF090
+#define SC_KEYMENU                      0xF100
+#define SC_ARRANGE                      0xF110
+#define SC_RESTORE                      0xF120
+#define SC_TASKLIST                     0xF130
+#define SC_SCREENSAVE                   0xF140
+#define SC_HOTKEY                       0xF150
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#define IDW_MAIN                        41
+#define IDW_ABOUT                       42
+#define IDW_VIEW_TOOLBAR                43
+#define IDW_VIEW_STATUSBAR              44
+#define IDW_READY                       45
+#define IDW_INDICATOR_CAPS              46
+#define IDW_INDICATOR_NUM               47
+#define IDW_INDICATOR_SCRL              48
+#define IDW_MENUBAR                     49
+#define IDW_TOOLBAR                     50
+#define IDW_QUIT                        51
+#define IDW_MDI_CASCADE                 52
+#define IDW_MDI_TILE                    53
+#define IDW_MDI_ARRANGE                 54
+#define IDW_MDI_CLOSEALL                55
+#define IDW_FIRSTCHILD                  56
+#define IDW_CHILD2                      57
+#define IDW_CHILD3                      58
+#define IDW_CHILD4                      59
+#define IDW_CHILD5                      60
+#define IDW_CHILD6                      61
+#define IDW_CHILD7                      62
+#define IDW_CHILD8                      63
+#define IDW_CHILD9                      64
+#define IDW_CHILD10                     65
+#define IDW_FILE_MRU_FILE1              66
+#define IDW_FILE_MRU_FILE2              67
+#define IDW_FILE_MRU_FILE3              68
+#define IDW_FILE_MRU_FILE4              69
+#define IDW_FILE_MRU_FILE5              70
+#define IDW_FILE_MRU_FILE6              71
+#define IDW_FILE_MRU_FILE7              72
+#define IDW_FILE_MRU_FILE8              73
+#define IDW_FILE_MRU_FILE9              74
+#define IDW_FILE_MRU_FILE10             75
+#define IDW_FILE_MRU_FILE11             76
+#define IDW_FILE_MRU_FILE12             77
+#define IDW_FILE_MRU_FILE13             78
+#define IDW_FILE_MRU_FILE14             79
+#define IDW_FILE_MRU_FILE15             80
+#define IDW_FILE_MRU_FILE16             81
+#define IDW_SPLITH                      82
+#define IDW_SPLITV                      83
+#define IDW_TRACK4WAY                   84
+#define IDW_SDBOTTOM                    85
+#define IDW_SDCENTER                    86
+#define IDW_SDLEFT                      87
+#define IDW_SDMIDDLE                    88
+#define IDW_SDRIGHT                     89
+#define IDW_SDTOP                       90
+#define IDW_PRINTPREVIEW                91
+#define IDW_PREVIEWPRINT                92
+#define IDW_PREVIEWSETUP                93
+#define IDW_PREVIEWPREV                 94
+#define IDW_PREVIEWNEXT                 95
+#define IDW_PREVIEWCLOSE                96
+#define IDW_PREVIEWPANE                 97
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#define IDB_TOOLBAR16                   101
+#define IDI_DOCKER                      102
+#define IDI_VIEW                        103
